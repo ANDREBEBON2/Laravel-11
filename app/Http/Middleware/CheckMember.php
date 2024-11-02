72695a881ebbbc0ce23ac2345cf31a0094ff886c
ANDREBEBON2 <andrebebon21@gmail.com>
2024-11-02 15:14:39 +0700

    menambahkan aksi SESUDAH dan SEBELUM request di middleware CheckMember

--- app/Http/Middleware/CheckMember.php
+++ app/Http/Middleware/CheckMember.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class CheckMember
@@ -19,6 +20,22 @@ class CheckMember
         if (!$request->member == true) {
             return redirect('/price'); // jika tidak maka redirect ke route /price
         }
-        return $next($request);
+
+        // Log info ini akan di simpan di file storege/logs/laravel.log
+        Log::info('Sebelum Request : ', [
+            "url" => $request->url(),
+            "params" => $request->all(),
+        ]);
+
+        $request = $next($request); // menyimpan hasil request sebelumnya di variabel $request
+
+        sleep(1); // menambahka sleep agar dapat melihat jeda antara sebelum dan sesudah request, hanya untuk debuging
+
+        Log::info('Sesudah Request : ', [
+            'status' => $request->getStatusCode(),
+            'content' => $request->getContent(),
+        ]);
+
+        return $request;
     }
 }
