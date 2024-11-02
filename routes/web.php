ca65242b829e4db627c1a5d2b1bb541ce572497c
ANDREBEBON2 <andrebebon21@gmail.com>
2024-11-02 13:44:17 +0700

    Membuat CRUD sederhana pada routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,70 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Materi route
+$movies = [];
+for ($i = 0; $i < 10; $i++) {
+    $movies[] = [
+        'judul' => 'Movie ' . $i,
+        'genre' => 'Comedi',
+        'tahun' => 'tahun',
+    ];
+}
+
+//GET
+Route::get('/movie', function () use ($movies) {
+    if (!empty($movies)) {
+        return $movies;
+    } else {
+        return response()->json('Data tidak tersedia !', 404);
+    }
+});
+
+//POST
+Route::post('/movie', function () {
+    if (isset($movies)) {
+        $movies[] = [
+            'judul' => request('judul'),
+            'genre' => request('genre'),
+            'tahun' => request('tahun'),
+        ];
+        return $movies;
+    } else {
+        return response()->json('Maaf data tidak tersedia !');
+    }
+});
+
+
+// PUT
+Route::put('/movie/{id}', function ($id) use ($movies) {
+    if (!empty($movies)) {
+        $movies[$id]['judul'] = request('judul');
+        $movies[$id]['genre'] = request('genre');
+        $movies[$id]['tahun'] = request('tahun');
+        return $movies;
+    } else {
+        return response()->json('Maaf data tidak tersedia !');
+    }
+});
+
+// PATCH
+Route::patch('/movie/{id}', function ($id) use ($movies) {
+    if (!empty($movies)) {
+        $movies[$id]['judul'] = request('judul');
+        $movies[$id]['tahun'] = request('tahun');
+        return $movies;
+    } else {
+        return response()->json('Maaf data tidak tersedia !');
+    }
+});
+
+// DELETE
+Route::delete('/movie/{id}', function ($id) use ($movies) {
+    if (!empty($movies)) {
+        unset($movies[$id]);
+        return $movies;
+    } else {
+        return response()->json('Maaf data tidak tersedia');
+    }
+});
