61a50c9afedd179f9ae4c3b6f16d1a752a89f1d2
ANDREBEBON2 <andrebebon21@gmail.com>
2024-11-02 16:54:38 +0700

    Membuat middleware isAuth dan menggunakan banyak middleware pada 1 route serta membuat route group di mana semua route dalam group  menggunakan middleware isAuth

--- routes/web.php
+++ routes/web.php
@@ -17,78 +17,87 @@ for ($i = 0; $i < 10; $i++) {
     ];
 }
 
-//GET
-Route::get('/movie', function () use ($movies) {
-    if (!empty($movies)) {
-        return $movies;
-    } else {
-        return response()->json('Data tidak tersedia !', 404);
+// route group memungkin semua route dapat menggunakan middleware yang sama
+Route::group(
+    [
+        'middleware' => ['isAuth'], //middleware yang di gunakan semua route pada group
+        'prefix' => 'movie', //url yang di tuju route jika '/' maka '/movie'
+        'as' => 'movie.' //nama dari route jika 'movie.' maka 'movie.namaRoute'
+    ],
+    function () use ($movies) {
+
+        //GET
+        Route::get('/', function () use ($movies) {
+            if (!empty($movies)) {
+                return $movies;
+            } else {
+                return response()->json('Data tidak tersedia !', 404);
+            }
+        })->name('GetAll');
+
+        //POST
+        Route::post('/', function () use ($movies) {
+            if (isset($movies)) {
+                $movies[] = [
+                    'judul' => request('judul'),
+                    'genre' => request('genre'),
+                    'tahun' => request('tahun'),
+                ];
+                return $movies;
+            } else {
+                return response()->json('Maaf data tidak tersedia !');
+            }
+        })->name('Post'); //memberikan nama pada route sehingga menjadi 'movie.Post'
+
+        // PUT
+        Route::put('/{id}', function ($id) use ($movies) {
+            if (!empty($movies)) {
+                $movies[$id]['judul'] = request('judul');
+                $movies[$id]['genre'] = request('genre');
+                $movies[$id]['tahun'] = request('tahun');
+                return $movies;
+            } else {
+                return response()->json('Maaf data tidak tersedia !');
+            }
+        })->name('Put');
+
+        // PATCH
+        Route::patch('/{id}', function ($id) use ($movies) {
+            if (!empty($movies)) {
+                $movies[$id]['judul'] = request('judul');
+                $movies[$id]['tahun'] = request('tahun');
+                return $movies;
+            } else {
+                return response()->json('Maaf data tidak tersedia !');
+            }
+        })->name('Patch');
+
+        // DELETE
+        Route::delete('/{id}', function ($id) use ($movies) {
+            if (!empty($movies)) {
+                unset($movies[$id]);
+                return $movies;
+            } else {
+                return response()->json('Maaf data tidak tersedia');
+            }
+        })->name('Delete');
+
+        // membuat route khusus member
+        Route::get('/{id}', function ($id) use ($movies) {
+            if (!empty($movies[$id])) {
+                return $movies[$id];
+            } else {
+                return response()->json('Maaf data yang anda cari tidak tersedia !', 404);
+            }
+        })->middleware('isMember')->name('GetById'); // memberikan tambahan middleware isMember pada route movie.GetById untuk cek apakah user adalah member atau bukan
+
     }
-});
-
-//POST
-Route::post('/movie', function () use ($movies) {
-    if (isset($movies)) {
-        $movies[] = [
-            'judul' => request('judul'),
-            'genre' => request('genre'),
-            'tahun' => request('tahun'),
-        ];
-        return $movies;
-    } else {
-        return response()->json('Maaf data tidak tersedia !');
-    }
-});
-
-
-// PUT
-Route::put('/movie/{id}', function ($id) use ($movies) {
-    if (!empty($movies)) {
-        $movies[$id]['judul'] = request('judul');
-        $movies[$id]['genre'] = request('genre');
-        $movies[$id]['tahun'] = request('tahun');
-        return $movies;
-    } else {
-        return response()->json('Maaf data tidak tersedia !');
-    }
-});
-
-// PATCH
-Route::patch('/movie/{id}', function ($id) use ($movies) {
-    if (!empty($movies)) {
-        $movies[$id]['judul'] = request('judul');
-        $movies[$id]['tahun'] = request('tahun');
-        return $movies;
-    } else {
-        return response()->json('Maaf data tidak tersedia !');
-    }
-});
-
-// DELETE
-Route::delete('/movie/{id}', function ($id) use ($movies) {
-    if (!empty($movies)) {
-        unset($movies[$id]);
-        return $movies;
-    } else {
-        return response()->json('Maaf data tidak tersedia');
-    }
-});
-
-
-// membuat route khusus member
-Route::get('/movie/{id}', function ($id) use ($movies) {
-    if (!empty($movies[$id])) {
-        return $movies[$id];
-    } else {
-        return response()->json('Maaf data yang anda cari tidak tersedia !', 404);
-    }
-})->middleware(['isAuth', 'isMember']); //2. pastikan alias sama seperti nama alias di app.php
-
+);
 
 // membuat route redirect middleware CheckMember
 Route::get('/price', function () {
     return response()->json("Silahkan melakukan pembelian terlebih dahulu !", 200);
-});
+})->middleware('isAuth');
 
 // membuat route redirect middleware isAuth
 Route::get("/login", function () {
